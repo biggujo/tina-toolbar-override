7d9ba69063bbd32f7362958086883c3db2fd3b3c
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-08-06 15:29:57 +0300

    Initial commit

---
+++ tina/collections/page.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('tinacms').Collection}
+ */
+export default {
+  label: "Page Content",
+  name: "page",
+  path: "content/page",
+  format: "mdx",
+  fields: [
+    {
+      name: "body",
+      label: "Main Content",
+      type: "rich-text",
+      toolbarOverride: ["heading","bold", "italic"],
+      isBody: true,
+    },
+  ],
+  ui: {
+    router: ({ document }) => {
+      if (document._sys.filename === "home") {
+        return `/`;
+      }
+      return undefined;
+    },
+  },
+};
